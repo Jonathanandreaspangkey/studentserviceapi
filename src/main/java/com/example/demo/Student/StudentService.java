69fc315f7220e5e46f9b489b3c10772e6f550f0d
Jonathan Pangkey <jonathanpangkey912@gmail.com>
2024-02-23 14:26:16 +0800

    build executable jar file

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
