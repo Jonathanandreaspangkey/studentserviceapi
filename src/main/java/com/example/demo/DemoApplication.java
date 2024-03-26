0c56dd82e8154cdc988c6fd1bad2f81fe86eba39
Jonathan Pangkey <jonathanpangkey912@gmail.com>
2024-03-26 14:36:46 +0800

    minor changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,12 +11,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-//		Student student = new Student(1L, "John Doe", "john@example.com", LocalDate.of(2000, 5, 15));
-//
-//		// Print out the details of the student using toString() method
-//		System.out.println("Student details:");
-//		System.out.println(student.toString());
 	}
 
 }
